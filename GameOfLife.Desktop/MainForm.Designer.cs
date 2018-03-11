6437414a4992dcabad0fcb3c5f0952567bb9608d
Jonas Ehlin <jonas.ehlin@joliv.se>
2018-03-11 17:50:59 +0100

    Add first version of world view

--- GameOfLife.Desktop/MainForm.Designer.cs
+++ GameOfLife.Desktop/MainForm.Designer.cs
@@ -28,13 +28,27 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this._worldView = new GameOfLife.Desktop.WorldView();
 			this.SuspendLayout();
+			// 
+			// _worldView
+			// 
+			this._worldView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._worldView.BackColor = System.Drawing.SystemColors.Window;
+			this._worldView.ForeColor = System.Drawing.Color.Blue;
+			this._worldView.Location = new System.Drawing.Point(216, 12);
+			this._worldView.Name = "_worldView";
+			this._worldView.Size = new System.Drawing.Size(364, 338);
+			this._worldView.TabIndex = 0;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(592, 362);
+			this.Controls.Add(this._worldView);
 			this.Name = "MainForm";
 			this.Text = "Game of Life";
 			this.ResumeLayout(false);
@@ -42,6 +56,8 @@
 		}
 
 		#endregion
+
+		private WorldView _worldView;
 	}
 }
 
---
+++ GameOfLife.Desktop/WorldView.cs
@@ -0,0 +1,95 @@
+﻿using GameOfLife.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife.Desktop
+{
+	public partial class WorldView : Control
+	{
+		private World _world;
+		private float _cellSize;
+		private Coordinate _topLeft;
+
+		public float CellSize
+		{
+			get { return _cellSize; }
+			set
+			{
+				_cellSize = value;
+			}
+		}
+
+		private Area VisibleArea
+		{
+			get
+			{
+				return new Area(
+					_topLeft.X,
+					_topLeft.Y,
+					(int)Math.Ceiling(ClientRectangle.Width / CellSize),
+					(int)Math.Ceiling(ClientRectangle.Height / CellSize));
+			}
+		}
+
+		public WorldView()
+		{
+			InitializeComponent();
+
+			_world = new World();
+
+			_world[10, 10] = true;
+			_world[10, 11] = true;
+			_world[10, 12] = true;
+			_world[11, 12] = true;
+			_world[12, 12] = true;
+			_world[12, 10] = true;
+			_world[11, 10] = true;
+
+			CellSize = 10.0f;
+
+			_worldTimer.Start();
+		}
+
+		protected override void OnPaint(PaintEventArgs pe)
+		{
+			base.OnPaint(pe);
+
+			DrawCells(pe.Graphics);
+		}
+
+		private RectangleF GetCellRectangle(Coordinate position)
+		{
+			return new RectangleF(
+				position.X * CellSize,
+				position.Y * CellSize,
+				CellSize,
+				CellSize);
+		}
+
+		private void DrawCells(Graphics gr)
+		{
+			var cells = _world.GetCells(VisibleArea);
+
+			using (var brush = new SolidBrush(ForeColor))
+			{
+				foreach (var cell in cells)
+				{
+					gr.FillRectangle(brush, GetCellRectangle(cell));
+				}
+			}
+		}
+
+		private void WorldTimer_Tick(object sender, EventArgs e)
+		{
+			_world.NextGeneration();
+			Invalidate();
+		}
+	}
+}
